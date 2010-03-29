2469bdf5b80951e6f033b16aa3bb5f06d719febe
Otto Hammersmith <otto.hammersmith@gmail.com>
2010-03-29 17:46:57 -0400

    Only executing on servers that would actually have nginx.

--- cookbooks/nginx_redirects/recipes/default.rb
+++ cookbooks/nginx_redirects/recipes/default.rb
@@ -3,10 +3,12 @@
 # Recipe:: default
 #
 
-remote_file "/data/nginx/servers/protopipe.rewrites" do
-    owner "deploy"
-    group "deploy"
-    mode 0644
-    source "nginx.rewrites"
-    action :create
+if ['solo', 'app', 'app_master'].include?(node[:instance_role])
+  remote_file "/data/nginx/servers/protopipe.rewrites" do
+      owner "deploy"
+      group "deploy"
+      mode 0644
+      source "nginx.rewrites"
+      action :create
+  end
 end
