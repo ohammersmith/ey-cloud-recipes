0e31f20acd6f210f94693f1d08b1d2eca0762587
Otto Hammersmith <otto.hammersmith@gmail.com>
2010-04-16 15:46:35 -0400

    Forgot to include the other kind of node and it seems the name conflict squashes the first cron job when writing out crontab.

--- cookbooks/protopipe_cronjobs/recipes/default.rb
+++ cookbooks/protopipe_cronjobs/recipes/default.rb
@@ -5,7 +5,7 @@
 
 # Cron jobs on just primary app nodes
 if ['solo', 'app_master'].include?(node[:instance_role])
-  cron "" do
+  cron "invites" do
     minute  '47'
     hour '4'
     command "(cd /data/protopipe/current ; rake invitations:send_to_all_matching_job_seekers RAILS_ENV=#{node[:environment][:framework_env]} )"
@@ -14,8 +14,8 @@ if ['solo', 'app_master'].include?(node[:instance_role])
 end
 
 # Cron jobs on all app nodes
-if ['solo', 'app_master'].include?(node[:instance_role])
-  cron "" do
+if ['solo', 'app_master', 'app'].include?(node[:instance_role])
+  cron "indexing" do
     minute  '47'
     hour '0'
     command "(cd /data/protopipe/current ; rake ts:in RAILS_ENV=#{node[:environment][:framework_env]})"
